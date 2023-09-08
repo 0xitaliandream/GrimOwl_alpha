4e02d372393a25a68479834fa1639563827a6c33
bigdrd <e.luciani@protonmail.com>
2023-09-08 18:00:59 +0200

    GrimOwlRiptideClient event OnNewGameUpdate

--- GrimOwlRiptideClient/GrimOwlClient.cs
+++ GrimOwlRiptideClient/GrimOwlClient.cs
@@ -1,34 +1,43 @@
-﻿using Riptide;
+﻿using GrimOwlGameEngine;
+using Riptide;
 
 namespace GrimOwlRiptideClient;
 
-public static class GrimOwlClient
+public class GrimOwlClient
 {
-    internal static Client? client;
-    internal static Thread? clientThread;
-    internal static bool isRunning = true;
+    public static GrimOwlClient Instance { get; private set; } = null!;
 
-    internal static string playerJwtAuthToken = null!;
-    internal static int serverIdToken;
+    internal Client? client;
+    internal Thread? clientThread;
+    internal bool isRunning = true;
 
-    public static void Run(string playerJwtAuthTokenInit, int serverIdTokenInit)
+    internal string playerJwtAuthToken = null!;
+    internal int serverIdToken;
+
+    public event Action<string> OnNewGameUpdate = delegate { };
+
+    public GrimOwlClient(string playerJwtAuthTokenInit, int serverIdTokenInit)
+    {
+        playerJwtAuthToken = playerJwtAuthTokenInit;
+        serverIdToken = serverIdTokenInit;
+
+        Instance = this;
+    }
+
+    public void Run()
     {
         Console.CancelKeyPress += delegate
         {
             Stop();
         };
 
-        playerJwtAuthToken = playerJwtAuthTokenInit;
-        serverIdToken = serverIdTokenInit;
-
-
         Console.WriteLine("GameClient created");
 
         client = new Client();
 
         Message msg_temp = Message.Create();
-        msg_temp.AddString(playerJwtAuthTokenInit);
-        msg_temp.AddInt(serverIdTokenInit);
+        msg_temp.AddString(playerJwtAuthToken);
+        msg_temp.AddInt(serverIdToken);
 
         client.Connect("127.0.0.1:5000", message: msg_temp);
         client.Send(msg_temp);
@@ -38,7 +47,7 @@ public static class GrimOwlClient
     }
 
 
-    public static void Tick()
+    public void Tick()
     {
         while (isRunning)
         {
@@ -48,10 +57,20 @@ public static class GrimOwlClient
         client?.Disconnect();
     }
 
-    public static void Stop()
+    public void Stop()
     {
         isRunning = false;
 
         clientThread?.Join();
+    }
+
+    [MessageHandler((ushort)MConnection.ServerHello)]
+    private static void GameState(Message message)
+    {
+        Console.WriteLine("Message received");
+
+        string test = message.GetString();
+
+        Instance.OnNewGameState?.Invoke(test);
     }
 }
---
+++ GrimOwlRiptideClient/MessageKeys.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrimOwlRiptideClient;
+
+
+
+public enum MConnection : ushort
+{
+    ServerHello = 1,
+}
+
+public enum MNotificator : ushort
+{
+    GameState = 100,
+    GameStarted,
+}
+
+public enum MClient : ushort
+{
+    ClientCommand = 200,
+}
+
+public enum MCommand : ushort
+{
+    Summon = 1000,
+    EndsTurn,
+    Move,
+    Attack,
+}
